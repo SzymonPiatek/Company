c03747c9c958a6ed659aa0014b4062b2dee37e22
Szymon Piątek <szymon.piatek125@gmail.com>
2025-04-16 14:30:06 +0200

    createResourceType.handler

--- api/resource-service/src/models/resourceType/resourceType.route.ts
+++ api/resource-service/src/models/resourceType/resourceType.route.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import getResourceTypesHandler from './handlers/getResourcesType.handler';
 import getResourceTypeByIdHandler from './handlers/getResourceTypeById.handler';
+import createResourceTypeHandler from './handlers/createResourceType.handler';
 
 const router = Router();
 
 router.get('/', getResourceTypesHandler);
 router.get('/:id', getResourceTypeByIdHandler);
+
+router.post('/', createResourceTypeHandler);
 
 export default router;
