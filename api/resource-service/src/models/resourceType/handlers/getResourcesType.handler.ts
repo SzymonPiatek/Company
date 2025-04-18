c67231db092531fd152dce63450c1e5fe17aa305
Szymon Piątek <szymon.piatek125@gmail.com>
2025-04-18 21:43:36 +0200

    Query for resource and resourceType

--- api/resource-service/src/models/resourceType/handlers/getResourcesType.handler.ts
+++ api/resource-service/src/models/resourceType/handlers/getResourcesType.handler.ts
@@ -2,14 +2,30 @@ import prisma from '../../../prismaClient';
 import paginateData from '../../../utils/helpers/paginateData';
 import parsePaginationQuery from '../../../utils/helpers/parsePaginationQuery';
 import type { RequestHandler } from 'express';
+import buildQueryConditions from '../../../utils/helpers/buildQueryConditions';
+
+type ResourceTypeQueryProps = {
+  name?: string;
+  code?: string;
+  search?: string;
+};
 
 const getResourceTypesHandler: RequestHandler = async (req, res) => {
   try {
     const pagination = parsePaginationQuery(req);
 
+    const { name, code, search } = req.query as ResourceTypeQueryProps;
+
+    const where = buildQueryConditions({
+      fields: ['name', 'code'],
+      filters: { name, code },
+      search,
+    });
+
     const result = await paginateData(
       prisma.resourceType,
       {
+        where,
         include: {
           resources: true,
         },
