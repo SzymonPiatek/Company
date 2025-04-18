66a3ecf76ed47da280b8864d76cdbd426dc78e1a
Szymon Piątek <szymon.piatek125@gmail.com>
2025-04-18 13:53:19 +0200

    Update resource type

---
+++ api/resource-service/src/models/resourceType/handlers/updateResourceType.handler.ts
@@ -0,0 +1,56 @@
+import type { RequestHandler } from 'express';
+import prisma from '../../../prismaClient';
+
+type UpdateResourceTypeProps = {
+  name: string;
+  code: string;
+};
+
+const updateResourceTypeHandler: RequestHandler<{ id: string }, unknown, UpdateResourceTypeProps> = async (req, res): Promise<void> => {
+  const { id } = req.params;
+  const { name, code } = req.body;
+
+  try {
+    const existingType = await prisma.resourceType.findUnique({ where: { id } });
+
+    if (!existingType) {
+      res.status(404).json({ error: 'ResourceType not found' });
+      return;
+    }
+
+    const nameTaken = await prisma.resourceType.findFirst({
+      where: {
+        name,
+        NOT: { id },
+      },
+    });
+
+    if (nameTaken) {
+      res.status(409).json({ error: 'Resource name already exists' });
+      return;
+    }
+
+    const codeTaken = await prisma.resourceType.findFirst({
+      where: {
+        code,
+        NOT: { id },
+      },
+    });
+
+    if (codeTaken) {
+      res.status(409).json({ error: 'Resource code already exists' });
+      return;
+    }
+
+    const updated = await prisma.resourceType.update({
+      where: { id },
+      data: { name, code },
+    });
+
+    res.status(200).json(updated);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error', details: error });
+  }
+};
+
+export default updateResourceTypeHandler;
--- api/resource-service/src/models/resourceType/resourceType.route.ts
+++ api/resource-service/src/models/resourceType/resourceType.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import getResourceTypesHandler from './handlers/getResourcesType.handler';
 import getResourceTypeByIdHandler from './handlers/getResourceTypeById.handler';
 import createResourceTypeHandler from './handlers/createResourceType.handler';
+import editResourcesTypeHandler from './handlers/updateResourceType.handler';
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.get('/', getResourceTypesHandler);
 router.get('/:id', getResourceTypeByIdHandler);
 
 router.post('/', createResourceTypeHandler);
+
+router.patch('/:id', editResourcesTypeHandler);
 
 export default router;
