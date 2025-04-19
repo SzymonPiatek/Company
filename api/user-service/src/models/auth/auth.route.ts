eff58e556c40ad775bacaeb0239990d278e15bb7
Szymon Piątek <szymon.piatek125@gmail.com>
2025-04-19 07:58:31 +0200

    Auth login handler

---
+++ api/user-service/src/models/auth/auth.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import loginHandler from './handlers/login.handler';
+
+const router = Router();
+
+router.post('/login', loginHandler);
+
+export default router;
--- api/user-service/src/utils/helpers/bcrypt.ts
+++ api/user-service/src/utils/helpers/bcrypt.ts
@@ -3,3 +3,7 @@ import bcrypt from 'bcryptjs';
 export const hashPassword = async (password: string) => {
   return await bcrypt.hash(password, 12);
 };
+
+export const comparePassword = async (firstPassword: string, secondPassword: string) => {
+  return await bcrypt.compare(firstPassword, secondPassword);
+};
