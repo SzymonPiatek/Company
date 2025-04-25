39dae58abb02d8559eb6c046ba0c2873f5586361
Szymon Piątek <szymon.piatek125@gmail.com>
2025-04-25 15:22:37 +0200

    Fix tests

--- api/libs/types/types.ts
+++ api/libs/types/types.ts
@@ -1,8 +1,8 @@
 export type SortOrder = "asc" | "desc";
 
 export interface PaginationParams {
-  page: number;
-  limit: number;
+  page?: number;
+  limit?: number;
   sortBy?: string;
   sortOrder?: SortOrder;
 }
