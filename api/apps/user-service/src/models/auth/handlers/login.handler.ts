716edd33f324cf6495855e6efc77a6bdd727d339
Szymon Piątek <szymon.piatek125@gmail.com>
2025-05-02 23:12:26 +0200

    Generating tokens (login)

--- api/apps/user-service/src/models/auth/handlers/login.handler.ts
+++ api/apps/user-service/src/models/auth/handlers/login.handler.ts
@@ -1,10 +1,47 @@
 import type { RequestHandler } from 'express';
 import prisma from '../../../prismaClient';
 import { comparePassword } from '@libs/helpers/bcrypt';
+import jwt, { type Secret, type SignOptions } from 'jsonwebtoken';
 
 type LoginBodyProps = {
   email: string;
   password: string;
+};
+
+type GenerateTokenProps = {
+  userId: string;
+  secret: Secret;
+  exp: SignOptions['expiresIn'];
+};
+
+type GenerateAccessOrRefreshTokenProps = {
+  userId: string;
+};
+
+const accessTokenSecret: Secret = process.env.ACCESS_TOKEN_SECRET!;
+const refreshTokenSecret: Secret = process.env.REFRESH_TOKEN_SECRET!;
+
+const accessTokenExp = process.env.ACCESS_TOKEN_EXP! as SignOptions['expiresIn'];
+const refreshTokenExp = process.env.REFRESH_TOKEN_EXP! as SignOptions['expiresIn'];
+
+const generateToken = ({ userId, secret, exp }: GenerateTokenProps): string => {
+  return jwt.sign({ userId }, secret, { expiresIn: exp });
+};
+
+const generateAccessToken = ({ userId }: GenerateAccessOrRefreshTokenProps) => {
+  return generateToken({
+    userId,
+    secret: accessTokenSecret,
+    exp: accessTokenExp,
+  });
+};
+
+const generateRefreshToken = ({ userId }: GenerateAccessOrRefreshTokenProps) => {
+  return generateToken({
+    userId,
+    secret: refreshTokenSecret,
+    exp: refreshTokenExp,
+  });
 };
 
 const loginHandler: RequestHandler = async (req, res) => {
@@ -30,8 +67,13 @@ const loginHandler: RequestHandler = async (req, res) => {
       return;
     }
 
+    const accessToken = generateAccessToken({ userId: user.id });
+    const refreshToken = generateRefreshToken({ userId: user.id });
+
     res.status(200).json({
       user: { ...user, password: undefined },
+      accessToken,
+      refreshToken,
     });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error', details: error });
